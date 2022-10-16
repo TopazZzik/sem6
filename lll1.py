04b3f674f68883b8a2df82fd66d239d112236626
Luda <millya92@gmail.com>
2022-10-16 23:22:34 +0300

    one1

--- lll1.py
+++ lll1.py
@@ -9,3 +9,16 @@
 #         print(a)
 #     else:
 #         print(a, end=', ')
+
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# list comprehension
+
+from itertools import accumulate
+import operator
+
+n = int(input('Введите число: '))
+
+list(accumulate([a for a in range(1, n + 1)], operator.mul))
+
+print(f"Набор произведений чисел от 1 до {n}: {list(accumulate([a for a in range(1, n + 1)], operator.mul))}")
