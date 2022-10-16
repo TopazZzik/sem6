80d7693189fddfd8c6b0b8f1782788442d6d6c8e
Luda <millya92@gmail.com>
2022-10-16 23:24:04 +0300

    two

---
+++ lll2.py
@@ -0,0 +1,15 @@
+# Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+
+# Number = int(input('Введите натуральное число > 0: '))
+# if Number > 0:
+#     result = {}
+#     total = 0
+#     for key in range(1, Number+1):
+#         result[key] = round((1+1/key)**key, 2)
+#         total += float(result[key])
+#     print(f"Список из {Number} чисел заданной последовательности (1+1/{Number})^{Number}: {result}")
+#     print(f"Cумма элементов из списка выше: {round(total, 2)}")
+# else:
+#     print("Ошибка ввода!")
+
+# lambda
